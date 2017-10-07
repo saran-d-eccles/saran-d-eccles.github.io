0c1464efcaeeee773b94b757120245275c136329
saran-d-eccles <32584570+saran-d-eccles@users.noreply.github.com>
2017-10-07 13:52:45 -0400

    Add files via upload

---
+++ js/drawscript.js
@@ -0,0 +1,159 @@
+//Code adapted from https://blog.alexdevero.com/learn-react-practice-create-gallery/
+
+'use strict';
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+// Cache gallery container
+var galleryContainer = document.querySelector('.react-gallery');
+
+// Create new array with URLs for images
+var imgUrls = ['../photos/1.jpg',
+              'https://source.unsplash.com/3Z70SDuYs5g/800x600',
+              'https://sites.google.com/site/jamrockreggaeband1/_/rsrc/1472844256430/config/customLogo.gif?revision=6',
+              'http://finaid.cornell.edu/sites/finaid.cornell.edu/files/styles/slide/public/Arch%20at%20Sunset%20webtest.jpg?itok=KFsG5E2X',
+              'http://webneel.com/daily/sites/default/files/images/daily/09-2013/22-most-amazing-photos-sunrise.jpg',
+              'http://webneel.com/daily/sites/default/files/images/daily/09-2013/17-most-amazing-photo-hight-sea-tide.jpg',
+              'http://webneel.com/daily/sites/default/files/images/daily/09-2013/4-amazing-photography-lake.jpg',
+              'http://f9view.com/wp-content/uploads/2013/04/amazing-animal-pics.jpg'];
+
+var descArray = ["Filler text ",
+              "Filler text Filler text ",
+              "Filler text Filler text Filler text ",
+              "Filler text Filler text Filler text Filler text ",
+              "Filler text Filler text Filler text Filler text Filler text ",
+              "Filler text Filler text Filler text Filler text Filler text Filler text ",
+              "Filler text Filler text Filler text Filler text Filler text Filler text Filler text ",
+              "Filler text Filler text Filler text Filler text Filler text Filler text Filler text Filler text "];
+
+// Component for gallery image
+
+var GalleryImage = function (_React$Component) {
+  _inherits(GalleryImage, _React$Component);
+
+  function GalleryImage() {
+    _classCallCheck(this, GalleryImage);
+
+    return _possibleConstructorReturn(this, _React$Component.apply(this, arguments));
+  }
+
+  GalleryImage.prototype.render = function render() {
+    return React.createElement('img', { className: this.props.className, src: this.props.src, alt: this.props.alt });
+  };
+
+  return GalleryImage;
+}(React.Component);
+
+// Component for gallery modal
+
+var GalleryModal = function (_React$Component2) {
+  _inherits(GalleryModal, _React$Component2);
+
+  function GalleryModal() {
+    _classCallCheck(this, GalleryModal);
+
+    return _possibleConstructorReturn(this, _React$Component2.apply(this, arguments));
+  }
+
+  GalleryModal.prototype.render = function render() {
+    if (this.props.isOpen === false) {
+      return null;
+    }
+
+    return React.createElement(
+      'div',
+      { isOpen: this.props.isOpen, className: 'modal-overlay', onClick: this.props.onClick, name: this.props.name },
+      React.createElement(
+        'div',
+        { className: 'modal-body' },
+        React.createElement(
+          'a',
+          { className: 'modal-close', href: '#', onClick: this.props.onClick },
+          React.createElement('span', { className: 'fa fa-times' })
+        ),
+        React.createElement('img', { src: this.props.src })
+      )
+    );
+  };
+
+  return GalleryModal;
+}(React.Component);
+
+// Component for gallery
+
+var Gallery = function (_React$Component3) {
+  _inherits(Gallery, _React$Component3);
+
+  function Gallery(props) {
+    _classCallCheck(this, Gallery);
+
+    var _this3 = _possibleConstructorReturn(this, _React$Component3.call(this, props));
+
+    _this3.state = {
+      showModal: false,
+      url: ''
+    };
+
+    _this3.openModal = _this3.openModal.bind(_this3);
+
+    _this3.closeModal = _this3.closeModal.bind(_this3);
+    return _this3;
+  }
+
+  Gallery.prototype.render = function render() {
+    var _this4 = this;
+
+    return React.createElement(
+      'div',
+      { refs: 'gallery-container', className: 'container-fluid gallery-container' },
+      React.createElement(
+        'div',
+        { className: 'row' },
+        imgUrls.map(function (url, index) {
+          return React.createElement(
+            'div',
+            { className: 'col-sm-6 col-md-3 col-xl-2' },
+            React.createElement(
+              'div',
+              { className: 'gallery-card' },
+              React.createElement(GalleryImage, { className: 'gallery-thumbnail', src: url, alt: 'Image number ' + (index + 1) }),
+              React.createElement('span', { className: 'card-icon-open fa fa-expand', value: url, onClick: function onClick(e) {
+                  return _this4.openModal(url, e);
+                } }),
+              React.createElement('p', {id: 'p '+index}, descArray[index])
+            )
+          );
+        })
+      ),
+      React.createElement(GalleryModal, { isOpen: this.state.showModal, onClick: this.closeModal, src: this.state.url })
+    );
+  };
+
+  // Function for opening modal dialog
+
+  Gallery.prototype.openModal = function openModal(url, e) {
+    this.setState({
+      showModal: true,
+      url: url
+    });
+  };
+
+  // Function for closing modal dialog
+
+  Gallery.prototype.closeModal = function closeModal() {
+    this.setState({
+      showModal: false,
+      url: ''
+    });
+  };
+
+  return Gallery;
+}(React.Component);
+
+// Let's render the whole thing
+
+ReactDOM.render(React.createElement(Gallery, { imgUrls: imgUrls }), galleryContainer);
---
+++ js/fotoscript.js
@@ -0,0 +1,159 @@
+//Code adapted from https://blog.alexdevero.com/learn-react-practice-create-gallery/
+
+'use strict';
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+// Cache gallery container
+var galleryContainer = document.querySelector('.react-gallery');
+
+// Create new array with URLs for images
+var imgUrls = ['../drawings/1.gif',
+              'https://cdn.pixabay.com/photo/2017/01/06/19/15/soap-bubble-1958650_1280.jpg',
+              'https://s3.amazonaws.com/cms.ipressroom.com/292/files/201604/57111e61dfc2a546ab14cba1_Alvin+Ailey+American+Dance+Theater%27s+Michael+Francis+McBride.+Photo+by+Andrew+Eccles/Alvin+Ailey+American+Dance+Theater%27s+Michael+Francis+McBride.+Photo+by+Andrew+Eccles_thmb.jpg',
+              'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Raspberries.jpg/450px-Raspberries.jpg',
+              'https://usathss.files.wordpress.com/2017/08/pledger.jpg?w=1000&h=600&crop=1',
+              'https://twistedsifter.files.wordpress.com/2016/07/dulmen_bornste_waldweg.jpg',
+              'https://static1.squarespace.com/static/54c8795ae4b0b946b8b23b7a/t/55249eaae4b04bc773d3f136/1428463285644/6-Tower+with+Lake-H%236E8C97C.jpg?format=1500w',
+              'http://i.imgur.com/PLwj35n.jpg'];
+
+var descArray = ["Filler text ",
+              "Filler text Filler text ",
+              "Filler text Filler text Filler text ",
+              "Filler text Filler text Filler text Filler text ",
+              "Filler text Filler text Filler text Filler text Filler text ",
+              "Filler text Filler text Filler text Filler text Filler text Filler text ",
+              "Filler text Filler text Filler text Filler text Filler text Filler text Filler text ",
+              "Filler text Filler text Filler text Filler text Filler text Filler text Filler text Filler text "];
+
+// Component for gallery image
+
+var GalleryImage = function (_React$Component) {
+  _inherits(GalleryImage, _React$Component);
+
+  function GalleryImage() {
+    _classCallCheck(this, GalleryImage);
+
+    return _possibleConstructorReturn(this, _React$Component.apply(this, arguments));
+  }
+
+  GalleryImage.prototype.render = function render() {
+    return React.createElement('img', { className: this.props.className, src: this.props.src, alt: this.props.alt });
+  };
+
+  return GalleryImage;
+}(React.Component);
+
+// Component for gallery modal
+
+var GalleryModal = function (_React$Component2) {
+  _inherits(GalleryModal, _React$Component2);
+
+  function GalleryModal() {
+    _classCallCheck(this, GalleryModal);
+
+    return _possibleConstructorReturn(this, _React$Component2.apply(this, arguments));
+  }
+
+  GalleryModal.prototype.render = function render() {
+    if (this.props.isOpen === false) {
+      return null;
+    }
+
+    return React.createElement(
+      'div',
+      { isOpen: this.props.isOpen, className: 'modal-overlay', onClick: this.props.onClick, name: this.props.name },
+      React.createElement(
+        'div',
+        { className: 'modal-body' },
+        React.createElement(
+          'a',
+          { className: 'modal-close', href: '#', onClick: this.props.onClick },
+          React.createElement('span', { className: 'fa fa-times' })
+        ),
+        React.createElement('img', { src: this.props.src })
+      )
+    );
+  };
+
+  return GalleryModal;
+}(React.Component);
+
+// Component for gallery
+
+var Gallery = function (_React$Component3) {
+  _inherits(Gallery, _React$Component3);
+
+  function Gallery(props) {
+    _classCallCheck(this, Gallery);
+
+    var _this3 = _possibleConstructorReturn(this, _React$Component3.call(this, props));
+
+    _this3.state = {
+      showModal: false,
+      url: ''
+    };
+
+    _this3.openModal = _this3.openModal.bind(_this3);
+
+    _this3.closeModal = _this3.closeModal.bind(_this3);
+    return _this3;
+  }
+
+  Gallery.prototype.render = function render() {
+    var _this4 = this;
+
+    return React.createElement(
+      'div',
+      { refs: 'gallery-container', className: 'container-fluid gallery-container' },
+      React.createElement(
+        'div',
+        { className: 'row' },
+        imgUrls.map(function (url, index) {
+          return React.createElement(
+            'div',
+            { className: 'col-sm-6 col-md-3 col-xl-2' },
+            React.createElement(
+              'div',
+              { className: 'gallery-card' },
+              React.createElement(GalleryImage, { className: 'gallery-thumbnail', src: url, alt: 'Image number ' + (index + 1) }),
+              React.createElement('span', { className: 'card-icon-open fa fa-expand', value: url, onClick: function onClick(e) {
+                  return _this4.openModal(url, e);
+                } }),
+              React.createElement('p', {id: 'p '+index}, descArray[index])
+            )
+          );
+        })
+      ),
+      React.createElement(GalleryModal, { isOpen: this.state.showModal, onClick: this.closeModal, src: this.state.url })
+    );
+  };
+
+  // Function for opening modal dialog
+
+  Gallery.prototype.openModal = function openModal(url, e) {
+    this.setState({
+      showModal: true,
+      url: url
+    });
+  };
+
+  // Function for closing modal dialog
+
+  Gallery.prototype.closeModal = function closeModal() {
+    this.setState({
+      showModal: false,
+      url: ''
+    });
+  };
+
+  return Gallery;
+}(React.Component);
+
+// Let's render the whole thing
+
+ReactDOM.render(React.createElement(Gallery, { imgUrls: imgUrls }), galleryContainer);
